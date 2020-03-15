eb6dc8dd4a1bcd3dfe93e9f9c4aab675e46f18d4
Sagar Gubbi <sagar.writeme@gmail.com>
2020-03-16 01:05:50 +0530

    Fixed spi_flash_size

--- blinky/build.sh
+++ blinky/build.sh
@@ -13,23 +13,23 @@ LDFLAGS="-Wl,--no-check-sections -Wl,--gc-sections -u call_user_start -Wl,-stati
 LDLIBS="-Wl,--start-group -lc -lgcc -lhal -lphy -lpp -lnet80211 -llwip -lwpa -lcrypto -lmain -ljson -lupgrade -lssl -lpwm -lsmartconfig -Wl,--end-group"
 
 
-xtensa-lx106-elf-gcc $CFLAGS $IFLAGS $LFLAGS $LDFLAGS blinky.c $LDLIBS -o eagle.app.out
+xtensa-lx106-elf-gcc $CFLAGS $IFLAGS $LFLAGS $LDFLAGS blinky.c $LDLIBS -o eagle.app.v6.out
 
-python -m esptool elf2image eagle.app.out
+#python -m esptool elf2image eagle.app.v6.out
 
 #xtensa-lx106-elf-gcc -Os -g -Wpointer-arith -Wundef -Werror -Wl,-EL -fno-inline-functions -nostdlib -mlongcalls -mtext-section-literals -ffunction-sections -fdata-sections -fno-builtin-printf -DICACHE_FLASH -DSPI_FLASH_SIZE_MAP=4 -I ./ -I ../include -I ../include/eagle -I ../driver_lib/include -L../lib -nostdlib -T../ld/eagle.app.v6.ld -Wl,--no-check-sections -Wl,--gc-sections -u call_user_start -Wl,-static -Wl,--start-group -lc -lgcc -lhal -lphy -lpp -lnet80211 -llwip -lwpa -lcrypto -lmain -ljson -lupgrade -lssl -lpwm -lsmartconfig blinky.c -Wl,--end-group -o eagle.app.v6.out
 
-#xtensa-lx106-elf-objcopy --only-section .text -O binary eagle.app.v6.out eagle.app.v6.text.bin
-#xtensa-lx106-elf-objcopy --only-section .data -O binary eagle.app.v6.out eagle.app.v6.data.bin
-#xtensa-lx106-elf-objcopy --only-section .rodata -O binary eagle.app.v6.out eagle.app.v6.rodata.bin
-#xtensa-lx106-elf-objcopy --only-section .irom0.text -O binary eagle.app.v6.out eagle.app.v6.irom0text.bin
+xtensa-lx106-elf-objcopy --only-section .text -O binary eagle.app.v6.out eagle.app.v6.text.bin
+xtensa-lx106-elf-objcopy --only-section .data -O binary eagle.app.v6.out eagle.app.v6.data.bin
+xtensa-lx106-elf-objcopy --only-section .rodata -O binary eagle.app.v6.out eagle.app.v6.rodata.bin
+xtensa-lx106-elf-objcopy --only-section .irom0.text -O binary eagle.app.v6.out eagle.app.v6.irom0text.bin
 
 
-#python $ESP8266_NONOS_SDK_PATH/tools/gen_appbin.py eagle.app.v6.out 0 0 0 0 0
-#mv eagle.app.flash.bin eagle.flash.bin
-#mv eagle.app.v6.irom0text.bin eagle.irom0text.bin
-#rm eagle.app.v6.*
+python $ESP8266_NONOS_SDK_PATH/tools/gen_appbin.py eagle.app.v6.out 0 0 0 4 0
+mv eagle.app.flash.bin eagle.flash.bin
+mv eagle.app.v6.irom0text.bin eagle.irom0text.bin
+rm eagle.app.v6.*
 
-#echo "eagle.flash.bin-------->0x00000"
-#echo "eagle.irom0text.bin---->0x10000"
+echo "eagle.flash.bin-------->0x00000"
+echo "eagle.irom0text.bin---->0x10000"
 
--- blinky/flash.sh
+++ blinky/flash.sh
@@ -8,6 +8,6 @@ ESP8266_NONOS_SDK_PATH='/home/sagar/ESP8266_NONOS_SDK-3.0'
 #python -m esptool --port /dev/ttyUSB0 write_flash --flash_mode qio 0x3fb000 $ESP8266_NONOS_SDK_PATH/bin/blank.bin 0x3fc000 $ESP8266_NONOS_SDK_PATH/bin/esp_init_data_default_v08.bin 0x3fe000 $ESP8266_NONOS_SDK_PATH/bin/blank.bin
 
 
-#python -m esptool --port /dev/ttyUSB0 --baud 460800 write_flash --flash_mode qio 0x00000 eagle.flash.bin 0x10000 eagle.irom0text.bin
-python -m esptool --port /dev/ttyUSB0 --baud 460800 write_flash --flash_mode qio 0x00000 eagle.app.out-0x00000.bin 0x10000 eagle.app.out-0x10000.bin
+python -m esptool --port /dev/ttyUSB0 --baud 460800 write_flash --flash_mode qio 0x00000 eagle.flash.bin 0x10000 eagle.irom0text.bin
+#python -m esptool --port /dev/ttyUSB0 --baud 460800 write_flash --flash_mode qio 0x00000 eagle.app.v6.out-0x00000.bin 0x10000 eagle.app.v6.out-0x10000.bin
 
